2acafd16a7def0a605705f034813b5200b8c3135
jimmy <jimmyleon2016a2018@gmail.com>
2021-03-12 01:18:43 -0500

    segundo avance

--- php/datos.php
+++ php/datos.php
@@ -1,8 +1,4 @@
-
-
 <?php 
-   
-
     $boton_guar="";
 
     $nom = $_GET["nombre"];
@@ -11,8 +7,6 @@
     $fec = $_GET["fecha"];
     $cla = $_GET["clave"];
   
-
-
     if(isset($_GET['boton_guar'])){
         $bd_hos="localhost";
         $bd_usu="root";
@@ -21,8 +15,20 @@
         $conn = mysqli_connect($bd_hos,$bd_usu,$bd_cla,$bd_nom);
         if(mysqli_connect_errno()){
             echo"Error en la conexion";
+            exit();
         }else{
-            echo"conexion exitosa";
+           # echo"conexion exitosa";
+           #$f=str_replace('-','/',date('Y-m-d',strtotime($fec))); canvia formato de fecha
+          
+            $sql ="INSERT INTO registro(nombre, apellido, correo, fecha, clave) VALUES('$nom','$ape','$cor','$fec','$cla')";
+            $re=mysqli_query($conn,$sql);
+            if($re==true){
+                echo"Ingreso exitoso";
+                #en este lugar debemos colocar la con sulta de los ingresados  SELECT COUNT(id) FROM registro
+                mysqli_close( $conn);# sierra la conexion
+            }else{
+                echo "Error al insertar";
+            }
         }
 
 
